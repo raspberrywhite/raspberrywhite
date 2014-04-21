bda93867a1ba60155adc269721b190b05985817f
Andrea Stagi <stagi.andrea@gmail.com>
2014-04-21 14:50:34 +0200

    Detect connection error from popmuzik, minfixes

--- popmuzik.py
+++ popmuzik.py
@@ -2,6 +2,7 @@ import player
 from player import Mp3Player, PlayerListener
 import time
 import requests
+from requests.exceptions import ConnectionError
 
 class Popmuzik():
 
@@ -10,7 +11,6 @@ class Popmuzik():
             self.__popmuzik = popmuzik
         def onStop(self):
             self.__popmuzik.onStop()
-            self.__popmuzik.requestSong()
 
     def __init__(self):
         self.__player = player.Mp3Player()
@@ -18,20 +18,27 @@ class Popmuzik():
         self.__player.attachListener(self.__listener)
 
     def requestSong(self):
-        r = requests.get('http://localhost:8000/songs/next')
-        if r.status_code == 200:
-            song_json = r.json()
-            if 'path' in song_json:
-                self.__player.play(song_json['path'])
-        elif r.status_code == 404:
-            time.sleep(10)
-            self.requestSong()
+        try:
+            r = requests.get('http://localhost:8000/songs/next')
+            if r.status_code == 200:
+                song_json = r.json()
+                if 'path' in song_json:
+                    self.__player.play(song_json['path'])
+            elif r.status_code == 404:
+                self.__onFetchingFail()
+        except ConnectionError:
+            self.__onFetchingFail()
+
+    def __onFetchingFail(self):
+        time.sleep(10)
+        self.requestSong()
 
     def start(self):
         self.requestSong()
 
     def onStop(self):
         self.__player.shutdown()
+        self.requestSong()
 
 if __name__ == '__main__':
     pop = Popmuzik()
