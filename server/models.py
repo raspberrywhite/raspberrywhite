91bca997ddef05f283bba128d8158959dfb24bd8
Andrea Stagi <stagi.andrea@gmail.com>
2014-06-25 16:26:05 +0200

    Added remaining models

--- server/models.py
+++ server/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 
 class User(models.Model):
-    id = models.SlugField(primary_key=True)
-    username = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
+    id = models.BigIntegerField(primary_key=True)
+    name = models.TextField(blank=True)
+    username = models.TextField(blank=True)
+    email = models.TextField(blank=True)
+    is_admin = models.BooleanField(default=False)
+    last_request = models.BigIntegerField()
+
+class Song(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    title = models.TextField(blank=True)
+    artist = models.TextField(blank=True)
+    path = models.TextField(blank=True)
+
+class Request(models.Model):
+    user = models.ForeignKey(User)
+    song = models.ForeignKey(Song)
+    priority = models.BigIntegerField()
