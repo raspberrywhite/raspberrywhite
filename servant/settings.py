de1cb413c2f897249929855af4dc3e6f9cc54f75
Andrea Stagi <stagi.andrea@gmail.com>
2014-06-26 06:27:37 +0200

    Added login with Facebook

--- servant/settings.py
+++ servant/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'server'
+    'server',
+    'social.apps.django_app.default'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -47,6 +48,30 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+   'django.contrib.auth.context_processors.auth',
+   'django.core.context_processors.debug',
+   'django.core.context_processors.i18n',
+   'django.core.context_processors.media',
+   'django.core.context_processors.static',
+   'django.core.context_processors.tz',
+   'django.contrib.messages.context_processors.messages',
+   'social.apps.django_app.context_processors.backends',
+   'social.apps.django_app.context_processors.login_redirect',
+)
+
+AUTHENTICATION_BACKENDS = (
+   'social.backends.facebook.FacebookOAuth2',
+   'social.backends.google.GoogleOAuth2',
+   'social.backends.twitter.TwitterOAuth',
+   'django.contrib.auth.backends.ModelBackend',
+)
+
+LOGIN_REDIRECT_URL = '/accounts/register/'
+
+SOCIAL_AUTH_FACEBOOK_KEY = '497136996989028'
+SOCIAL_AUTH_FACEBOOK_SECRET = 'a93971a44f9b575bebed9b1b942b2dd1'
 
 ROOT_URLCONF = 'servant.urls'
 
--- servant/urls.py
+++ servant/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'servant.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^admin/', include(admin.site.urls)),
     (r'^accounts/login/$',  login),
     (r'^accounts/register/$',  register),
