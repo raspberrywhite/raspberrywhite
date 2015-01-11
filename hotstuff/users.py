1aa87dff663b49ff3edd76809d79ee2f6de875b7
Andrea Stagi <stagi.andrea@gmail.com>
2015-01-11 19:24:26 +0100

    Make hotstuff lib dealing with seconds

--- hotstuff/users.py
+++ hotstuff/users.py
@@ -10,12 +10,12 @@ def calc_priority(timelastreq, timeactualreq):
 Calculate user priority now
 """
 def calc_priority_now(timelastreq):
-    millis = int(round(time.time() * 1000))
+    millis = int(round(time.time()))
     return calc_priority(timelastreq, millis)
 
 """
 Calculate last request
 """
 def calc_last_request():
-    millis = int(round(time.time() * 1000))
+    millis = int(round(time.time()))
     return millis
