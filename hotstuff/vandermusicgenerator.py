5d4be20831be3cac08ec603a7ab40591bb6c03ae
Andrea Stagi <stagi.andrea@gmail.com>
2015-01-22 08:00:03 +0100

    Fix vandermusicgenerator library

--- hotstuff/vandermusicgenerator.py
+++ hotstuff/vandermusicgenerator.py
@@ -1,23 +1,21 @@
 import os
 import eyed3
+from server import models
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "servant.settings")
 
-from server import models
-
-# From now onwards start your script..
-
-mypath = 'songs/'
-
-for f in os.listdir(mypath):
-    path = os.path.join(mypath,f)
-    af = eyed3.load(path)
-    artist = af.tag.artist
-    title = af.tag.title
-    if models.Song.objects.filter(title = title, artist = artist).exists():
-        continue
-    song = models.Song()
-    song.path = path
-    song.title = title
-    song.artist = artist
-    song.save()
+def generate(songs_path):
+    for f in os.listdir(songs_path):
+        path = os.path.join(songs_path,f)
+        af = eyed3.load(path)
+        if not af:
+            continue
+        artist = af.tag.artist
+        title = af.tag.title
+        if models.Song.songs.filter(title = title, artist = artist).exists():
+            continue
+        song = models.Song()
+        song.path = path
+        song.title = title
+        song.artist = artist
+        song.save()
